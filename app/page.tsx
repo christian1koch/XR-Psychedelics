22cbc415e1fc37b1572889c3b151ce78b6537c1e
Christian Koch Echeverria <s0566264@htw-berlin.de>
2025-12-04 08:36:22 +0100

    Added Initial XR Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-export default function Home() {
+"use client";
+import { Canvas } from "@react-three/fiber";
+import { XR, createXRStore } from "@react-three/xr";
+import { useState } from "react";
+
+const store = createXRStore();
+
+export default function App() {
+    const [red, setRed] = useState(false);
     return (
-        <div className="bg-background">
-            <h1 className="mt-10 text-center text-4xl font-bold">
-                Hello, world!
-            </h1>
-        </div>
+        <>
+            <button onClick={() => store.enterAR()}>Enter AR</button>
+            <Canvas>
+                <XR store={store}>
+                    <mesh onClick={() => setRed(!red)} position={[0, 1, -1]}>
+                        <boxGeometry />
+                        <meshBasicMaterial color={red ? "red" : "blue"} />
+                    </mesh>
+                </XR>
+            </Canvas>
+        </>
     );
 }
